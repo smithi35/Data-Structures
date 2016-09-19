b30242e90a9fa3b293dd2b0f1558a1fb3d65675c
Ian Smith <ian_smith_12a@mymts.net>
2016-09-19 13:24:13 -0500

    Ready for testing.

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,7 +1,8 @@
 #include Node.h
 #include Queue.h
 
-Node::Node(int data) {
+Node::Node(int data)
+{
 	this.data = data;
 	next = null;
 }
@@ -12,16 +13,47 @@ Node *Node::get_next() { return next; }
 
 void Node::set_next(Node *node) { next = node; }
 
-Node::~Node() {
+Node::~Node()
+{
 	next = null;
 }
 
-Queue::Queue() {
+Queue::Queue()
+{
 	length = 0;
 	first = last = null;
 }
 
-void Queue::insert(int number) {
+void Queue::insert(int number)
+{
 	Node *newNode = new Node(number);
 	newNode.set_next(first);
+	length++;
+}
+
+int Queue::removeTop()
+{
+	Node *remove = first;
+	first = first.get_next();
+	length--;
+	return remove.get_data();
+}
+
+int Queue::search(int index)
+{
+	int currIndex = 0;
+	int value = -1;
+	Node *curr = first;
+	
+	if (index < length)
+	{
+		while (currIndex < index)
+		{
+			curr = curr.get_next();
+			index++;
+		}
+		value = curr.get_data();
+	}
+	
+	return value;
 }
